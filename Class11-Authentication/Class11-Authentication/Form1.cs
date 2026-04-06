e7d365eef3b0e4ac4934ef5d1395afc13f5b4e65
me-ayeshamoazzam <motiongraphicsbyayesha@gmail.com>
2026-04-06 15:33:34 +0500

    Added Authentication work

--- Class11-Authentication/Class11-Authentication/Form1.cs
+++ Class11-Authentication/Class11-Authentication/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -19,6 +20,30 @@ namespace Class11_Authentication
 
         private void button1_Click(object sender, EventArgs e)
         {
+            string name = textBox2.Text ;
+            string age = textBox3.Text ;
+
+            string connectionString = "Data Source=DESKTOP-VQDPLPC;Initial Catalog=login;Integrated Security=True";
+            SqlConnection con = new SqlConnection(connectionString);
+            string query = "SELECT COUNT(*) FROM users WHERE name=@name AND age=@age";
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@name", name);
+            cmd.Parameters.AddWithValue("@age", age);
+
+            con.Open();
+
+            int count = (int)cmd.ExecuteScalar();
+
+            con.Close();
+
+            if (count > 0)
+            {
+                MessageBox.Show("Login Successful", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Invalid name or age", "Login Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
 
         }
     }
