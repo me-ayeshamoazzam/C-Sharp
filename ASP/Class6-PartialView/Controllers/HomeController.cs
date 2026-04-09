42aefc5c4dd7828a6283e9eb5d0b97943894bc16
me-ayeshamoazzam <motiongraphicsbyayesha@gmail.com>
2026-04-10 03:16:38 +0500

    Added Class 6 folder

---
+++ ASP/Class6-PartialView/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+using Class6_PartialView.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Class6_PartialView.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Services()
+        {
+            return View();
+        }
+
+        public IActionResult Portfolio()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            });
+        }
+    }
+}
