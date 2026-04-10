3b313eaf42f7858fd2b1c8057e26ad3e08771cfe
me-ayeshamoazzam <motiongraphicsbyayesha@gmail.com>
2026-04-10 17:59:48 +0500

    Added Class 7 folder

---
+++ ASP/Class7-Validation/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Class7_Validation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult Index(string username, string age, string email)
+        {
+            if(string.IsNullOrWhiteSpace(username))
+            {
+                ModelState.AddModelError("username", "Username is required");
+            }
+            if(string.IsNullOrWhiteSpace(age))
+            {
+                ModelState.AddModelError("age", "Age is required");
+            }
+            if(string.IsNullOrWhiteSpace(email))
+            {
+                ModelState.AddModelError("email", "Email is required");
+            }
+            if(ModelState.IsValid == true)
+            {
+                ViewData["SuccessMessage"] = "<script> alert('Form Submitted') </script>";
+                ModelState.Clear();
+            }
+            return View();
+        }
+    }
+}
---
+++ ASP/Class7-Validation/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Class7_Validation.MvcApplication" Language="C#" %>
